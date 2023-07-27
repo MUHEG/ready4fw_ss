977c5aac3c3f33dc470d1ddd8476fe820ac2de71
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2023-07-27 16:01:33 +1000

    reworked abstract

--- setup.R
+++ setup.R
@@ -13,7 +13,7 @@ X <- Ready4showSynopsis(#background_1L_chr = "Our study is entirely fictional.",
                         keywords_chr = c("open source models","health economics","mental disorders"),
                         outp_formats_chr = "PDF",
                         #sample_desc_1L_chr = "The study sample is fake data that pretends to be young people aged 12 to 25 years who attended Australian primary care services for mental health related needs between November 2019 to August 2020.",
-                        title_1L_chr = "A prototype software framework for ethical implementation of computational economic models and its early application in youth mental health")
+                        title_1L_chr = "A prototype software framework for transparent, reusable and updatable health economic models")
 X <- renewSlot(X,
                "authors_r3", 
                {
@@ -37,13 +37,15 @@ X <- renewSlot(X,
                "abstract_args_ls",
                {
                  args_ls <- X@abstract_args_ls
-                 args_ls$abstract_ls <- list(Summary = "Understanding of what constitutes ethical computational implementation of health economic models is underdeveloped and software frameworks to facilitate such implementations are required. 
-                                             We propose criteria for assessing the ethical implementation of computational health economic models (CHEMs), describe a novel prototype software framework for developing CHEMs that meet these criteria and outline how we are using this software framework to develop and synthesise multiple economic models in youth mental health. 
-                                             We propose a total of six assessment criteria – two each for enabling transparent, reusable and updatable (TRU) CHEMs. 
-                                             The software framework is comprised of six R libraries that provide a toolkit for authoring CHEMs, supplying CHEMs with data and using CHEMs to implementing reproducible modelling analyses. 
-                                             The framework libraries integrate with existing online services for collaborative software development and data archiving. One of the initial applications of the software framework was to develop and apply utility mapping models in youth mental health. 
-                                             We assess the utility mapping CHEM as wholly meeting four of our ethical assessment criteria and partially meeting the remaining two criteria. 
-                                             We discuss how the ethical assessment criteria we have identified and the software framework we have developed can inform future work to develop understanding and implementation of ethical computational modelling practice in health economics.",
+                 args_ls$abstract_ls <- list(Summary = "Computers are tools that are now essential to the work of health economists. 
+                                             However, the ethical dimensions of alternative approaches to the computational implementation of health economic models are poorly understood. 
+                                             There is a corresponding need for technical infrastructure that can better facilitate implementations of computational health economic models (CHEMs) that meet explicit ethical standards. 
+                                             We propose six criteria for assessing ethical implementation of CHEMs – two each for the three domains of transparency, reusability and updatability. 
+                                             To facilitate the implementation of CHEMs that meet these criteria, we developed a novel prototype software framework in the open source programming language R. 
+                                             The framework comprises six code libraries that collectively provide a toolkit for authoring CHEMs, supplying them with data and using them to undertake reproducible analyses.  
+                                             The framework supports integrations with existing digital services for collaborative software development and data archiving. We are currently applying the software framework to develop and apply utility mapping models in youth mental health. 
+                                             We assess the first set of utility mapping CHEMs that we have developed with the framework as wholly meeting both transparency criteria (open access code and data and clarity about author contributions and beliefs), one reusability criteria (liberal terms of use) and one updatability criteria (infrastructure for model maintenance) and partially meeting the two remaining criteria for transparency (supports generalisability and transferability) and updatability (retesting and deprecation).  
+                                             The assessment criteria and the software framework we have developed can inform future work to understand and improve ethical computational implementations of health economic models.",
                                              # Objectives = "We aimed to develop a conceptual and technical framework for implementing open source mental health systems models and illustrate how it can be applicatied to economic topics in youth mental health.",
                                              # Methods = "We created a software development kit in R to support standardised implementation of mental health systems models. As a worked example, we applied the toolkit to develop open source youth mental health utility mapping models, which we assessed against five principles using 17 standards.",
                                              # Results = "Six R packages and a coding style dataset provide a toolkit for implementing mental health models that are TIMELY - Transparent, Iterative, Modular, Epitomising, Licensed and Yielding. The utility mapping models we developed with this toolkit fully met X and partially met Y TIMELY standards.", 
@@ -55,7 +57,7 @@ X <- renewSlot(X,
 # authorData(X) # Update ready4show so that default is to not overwrite Child_RMDs / only overwrite header and abstract
 # Pause for interactivity
 # Make manual edits of Markdown files
-authorReport(X)
+authorReport(X, consent_1L_chr = "Y")
 ##
 ##
 Y <- X
