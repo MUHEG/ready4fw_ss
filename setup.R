4a292f708e1efa0b2bbd95cf57e801ff0abba940
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2024-01-24 17:17:36 +1100

    extracted ethics component

--- setup.R
+++ setup.R
@@ -10,7 +10,7 @@ X <- Ready4showSynopsis(#background_1L_chr = "Our study is entirely fictional.",
                         ethics_1L_chr = "Software framework development did not involve human subject research and was not ethically reviewed. The utility mapping worked example is a previously reported study that was reviewed and granted approval by the University of Melbourne’s Human Research Ethics Committee, and the local Human Ethics and Advisory Group (1645367.1).",
                         funding_1L_chr = "Software framework development was funded by Orygen, VicHealth, Victoria University and an Australian Government Research Training Program (RTP) Scholarship. The utility mapping study used as a worked example was funded by the National Health and Medical Research Council (NHMRC, APP1076940), Orygen and headspace.",
                         #interval_chr = "three months",
-                        keywords_chr = c("computational models", "ethics of modelling", "health economics", "mental disorders", "open-source models", "software frameworks"),
+                        keywords_chr = c("computational models", "health economics", "mental disorders", "open-source models", "software frameworks"),
                         outp_formats_chr = "PDF",
                         #sample_desc_1L_chr = "The study sample is fake data that pretends to be young people aged 12 to 25 years who attended Australian primary care services for mental health related needs between November 2019 to August 2020.",
                         title_1L_chr = "A prototype software framework for transparent, reusable and updatable computational health economic models")
@@ -37,17 +37,11 @@ X <- renewSlot(X,
                "abstract_args_ls",
                {
                  args_ls <- X@abstract_args_ls
-                 args_ls$abstract_ls <- list(Summary = "Most health economic analyses are undertaken with the aid of computers. However, the ethical dimensions of implementing health economic models as software (or computational health economic models (CHEMs)) are poorly understood. 
-                                                        We propose that developers and funders of CHEMs share ethical responsibilities to (i) establish socially acceptable user requirements and design specifications; (ii) ensure fitness for purpose; and (iii) support socially beneficial use. 
-                                                        We further propose that a transparent (T), reusable (R) and updatable (U) CHEM is suggestive of a project team that has largely fulfilled these responsibilities.
-                                                        We propose six criteria for assessing CHEMs: (T1) software files are open access; (T2) project team contributions and judgments are easily identified; 
-                                                        (R1) programming practices promote generalisability and transferability; (R2) licenses restrict only unethical reuse; (U1) maintenance infrastructure is in place; and 
-                                                        (U2) new releases are systematically retested and appropriately deprecated.
-                                                        To facilitate CHEMs that meet TRU criteria, we have developed a prototype software framework in the open-source programming language R.
+                 args_ls$abstract_ls <- list(Summary = "To facilitate Computational Health Economic Models (CHEMs) that are Transparent, Reusable and Updatable (TRU), we have developed a prototype software framework in the open-source programming language R.
                                                         The framework comprises six code libraries for authoring CHEMs, supplying CHEMs with data and undertaking analyses with CHEMs. 
                                                         The prototype software framework integrates with services for software development and research data archiving. 
-                                                        We determine that an initial set of youth mental health CHEMs we developed with the prototype software framework wholly meet criteria T1-2, R1-2 and U1 and partially meet criterion U2.
-                                                        Our assessment criteria and prototype software framework can help inform and improve ethical implementation of CHEMs. Resource barriers to ethical CHEM practice should be addressed by research funders.",
+                                                        We used the framework to developed an initial set of utility mapping CHEMs in youth mental health that can be assessed as meeting explicit TRU criteria.
+                                                        Our software framework has potential value as a prototype for future tools to support ethical implementation of CHEMs.",
                                              # Objectives = "We aimed to develop a conceptual and technical framework for implementing open source mental health systems models and illustrate how it can be applicatied to economic topics in youth mental health.",
                                              # Methods = "We created a software development kit in R to support standardised implementation of mental health systems models. As a worked example, we applied the toolkit to develop open source youth mental health utility mapping models, which we assessed against five principles using 17 standards.",
                                              # Results = "Six R packages and a coding style dataset provide a toolkit for implementing mental health models that are TIMELY - Transparent, Iterative, Modular, Epitomising, Licensed and Yielding. The utility mapping models we developed with this toolkit fully met X and partially met Y TIMELY standards.", 
