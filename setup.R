548f9fd3616870504b65fc0d85c1131440a5e02b
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2024-02-26 07:06:08 +1100

    abstract updated

--- setup.R
+++ setup.R
@@ -37,16 +37,20 @@ X <- renewSlot(X,
                "abstract_args_ls",
                {
                  args_ls <- X@abstract_args_ls
-                 args_ls$abstract_ls <- list(Summary = "To facilitate Computational Health Economic Models (CHEMs) that are Transparent, Reusable and Updatable (TRU), we have developed a prototype software framework in the open-source programming language R.
-                                                        The framework comprises six code libraries for authoring CHEMs, supplying CHEMs with data and undertaking analyses with CHEMs. 
-                                                        The prototype software framework integrates with services for software development and research data archiving. 
-                                                        We used the framework to developed an initial set of utility mapping CHEMs in youth mental health that can be assessed as meeting explicit TRU criteria.
-                                                        Our software framework has potential value as a prototype for future tools to support ethical implementation of CHEMs.",
+                 args_ls$abstract_ls <- list(Summary = "We are developing an open source and modular Computational Health Economic Model (CHEM) in youth mental health.
+                                                        In order for that CHEM to be readily transferable to multiple jurisdictions, we aim for it to meet six explicit transparency, reusability and updatability (TRU) criteria.
+                                                        These criteria are: (T1) open access software files; (T2) easily identifiable developer contributions and judgments; (R1) programming practices promote selective reuse; (R2) permissions for derivative works; (U1) maintenance infrastructure; and (U2) systematic retesting and deprecation.
+                                                        To overcome technical challenges to achieving this goal, we developed a prototype software framework.
+                                                        We specified framework user requirements for a template CHEM module, a simple programming syntax and tools for authoring new CHEM modules, supplying CHEMs with data, reporting reproducible analyses, searching for CHEM modules and maintaining a project website.
+                                                        We developed the framework as six development version code libraries in the open-source programming language R that integrate with online services for software development and research data archiving. 
+                                                        We used the framework to author five development version R libraries of utility mapping CHEM modules in youth mental health that provide tools for variable validation, dataset description, multi-attribute instrument scoring, construction of mapping models, reporting of mapping studies and making out of sample predictions.
+                                                        We assessed these utility mapping modules as meeting TRU criteria T1, R1-2 and U1. The remaining two TRU criteria are partially met, but require more detailed documentation (for T2) and more unit testing (for U2).
+                                                        Our software framework has potential value as a prototype for future tools to support the development of transferable CHEMs.",
                                              # Objectives = "We aimed to develop a conceptual and technical framework for implementing open source mental health systems models and illustrate how it can be applicatied to economic topics in youth mental health.",
                                              # Methods = "We created a software development kit in R to support standardised implementation of mental health systems models. As a worked example, we applied the toolkit to develop open source youth mental health utility mapping models, which we assessed against five principles using 17 standards.",
                                              # Results = "Six R packages and a coding style dataset provide a toolkit for implementing mental health models that are TIMELY - Transparent, Iterative, Modular, Epitomising, Licensed and Yielding. The utility mapping models we developed with this toolkit fully met X and partially met Y TIMELY standards.", 
                                             #Conclusion = "",
-                                             Code = "Visit https://www.ready4-dev.com for more information about how to find, install and apply the prototype software framework and CHEMs developed with it.")
+                                             Code = "Visit https://www.ready4-dev.com for more information about how to find, install and apply the prototype software framework.")
                  args_ls
                  }
                )
